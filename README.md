fe527e5e21fb10a1963d556d81190dd57a50f4b3
Femil Savaliya <femilsavaliya555@gmail.com>
2022-02-02 22:05:13 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# FléXHub
+
+FléXHub is a Responsive React Movies App With API
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install FléXHub.
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+npm run start
+```
+
+## Resource
+
+```bash
+Google font: https://fonts.google.com/
+
+Boxicons: https://boxicons.com/
+
+Images: https://unsplash.com/
+
+API: https://www.themoviedb.org/
+```
+
+## Preview
+
+!["Responsive React Movies App With API"](https://user-images.githubusercontent.com/67447840/136721197-be990f3a-cae2-4757-9eb7-c0796f4a3834.png 'Responsive React Movies App With API')
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png';
+
+const Footer = () => {
+	return (
+		<div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+			<div className="footer__content container">
+				<div className="footer__content__logo">
+					<div className="logo">
+						<img src={logo} alt="" />
+						<Link to="/">FléXHub</Link>
+					</div>
+				</div>
+				<div className="footer__content__menus">
+					<div className="footer__content__menu">
+						<Link to="/">Home</Link>
+						<Link to="/">Contact us</Link>
+						<Link to="/">Term of services</Link>
+						<Link to="/">About us</Link>
+					</div>
+					<div className="footer__content__menu">
+						<Link to="/">Live</Link>
+						<Link to="/">FAQ</Link>
+						<Link to="/">Premium</Link>
+						<Link to="/">Pravacy policy</Link>
+					</div>
+					<div className="footer__content__menu">
+						<Link to="/">You must watch</Link>
+						<Link to="/">Recent release</Link>
+						<Link to="/">Top IMDB</Link>
+					</div>
+				</div>
+			</div>
+			<div className="footer__github">
+				<a href="amazon.com" className="footer__github__link">
+					<p>Fork this project</p>
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffffff">
+						<path d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
+					</svg>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
